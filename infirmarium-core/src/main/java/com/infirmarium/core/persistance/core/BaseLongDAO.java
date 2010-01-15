f7f8a47289d911a889763a113b91727d3dbf951c
yarcoh <yarcoh@f08a8d54-1206-11de-aa17-9100d24c02a7>
2010-01-15 22:24:08 +0000

    created a tag for the initial infirmarium version

--- infirmarium-core/src/main/java/com/infirmarium/core/persistance/core/BaseLongDAO.java
+++
@@ -1,6 +0,0 @@
-package com.infirmarium.core.persistance.core;
-
-public abstract class BaseLongDAO<E extends BaseLongEntity> extends
-	BaseDAO<E, Long> {
-
-}
--- infirmarium-core/src/main/java/com/infirmarium/core/services/IAddressService.java
+++
@@ -1,9 +0,0 @@
-package com.infirmarium.core.services;
-
-import com.infirmarium.core.persistance.core.IBaseService;
-import com.infirmarium.core.persistance.dao.AddressDAO;
-import com.infirmarium.core.persistance.domain.Address;
-
-public interface IAddressService extends IBaseService<Address, AddressDAO> {
-
-}
