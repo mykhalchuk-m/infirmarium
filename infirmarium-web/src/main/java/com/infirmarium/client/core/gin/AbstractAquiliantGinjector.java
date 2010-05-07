d072925711ab285cb1260daf6c1461f17d054312
yarcoh <yarcoh@f08a8d54-1206-11de-aa17-9100d24c02a7>
2010-05-07 18:28:50 +0000

    added injectMember method to AbstractAquiliantGinjector

--- infirmarium-web/src/main/java/com/infirmarium/client/core/gin/AbstractAquiliantGinjector.java
+++ infirmarium-web/src/main/java/com/infirmarium/client/core/gin/AbstractAquiliantGinjector.java
@@ -4,14 +4,20 @@ import net.customware.gwt.dispatch.client.DefaultDispatchAsync;
 import net.customware.gwt.presenter.client.EventBus;
 
 import com.google.gwt.inject.client.Ginjector;
+import com.infirmarium.client.components.elements.screens.DepartmentScreen;
+import com.infirmarium.client.components.elements.screens.PatientScreen;
 import com.infirmarium.client.internationalization.InfirmariumMessages;
 
-
 public interface AbstractAquiliantGinjector extends Ginjector {
+
+	void injectMembers(DepartmentScreen departmentScreen);
+
+	void injectMembers(PatientScreen patientScreen);
 
 	EventBus getEventBus();
 
 	DefaultDispatchAsync getDefaultDispatchAsync();
 
 	InfirmariumMessages InfirmariumMessages();
+
 }
